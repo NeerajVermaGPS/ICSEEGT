51310734fbacc4f235b01ee501aac18ca830b04f
Neeraj Verma <vermaneerajgreatpower@gmail.com>
2025-07-08 09:11:57 +0530

    update: Adds CFP, and register pages and prepares template for all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,127 +4,128 @@ LinkedIn: https://www.linkedin.com/in/neerajvermagps/
 Updated on: 07-07-2025
 */
 
-const menuIcon = document.querySelector(".menu-nav-ico");
-const menuNav = document.querySelector(".menu-nav");
-const mainNav = document.querySelector(".main-nav");
-const menuHeader = document.querySelector(".main-header");
-const navLinks = document.querySelectorAll(".menu-line");
-const logo = document.querySelector(".logo img");
-const launchDate = new Date(2025, 11, 12, 0, 0, 0);
-const countdownElement = document.querySelector(".countdown-blocks");
+import { header, aside, footer } from "/assets/components/template.js";
+
+const headerEl = document.getElementById("main-header");
+const asideEl = document.getElementById("main-aside");
+const footerEl = document.getElementById("main-footer");
 
 // Navbar
-menuIcon.addEventListener("click", function () {
-  for (let i = 0; i < 3; i++) {
-    navLinks[i].classList.toggle("animateMenu");
-  }
-  mainNav.classList.toggle("nav-height");
-  menuNav.classList.toggle("mhbg");
-  menuHeader.classList.toggle("header-height")
-});
-
-function menuBG() {
-	if(window.innerWidth < 850) {
-		if (window.scrollY > 50) {
-			menuHeader.style.background = "rgba(0, 0, 0, 0.7)";
-		} else {
-			menuHeader.style.background = "transparent";
-		}
-	}
-}
+const createMenu = () => {
+  const menuIcon = document.querySelector(".menu-nav-ico");
+  const menuNav = document.querySelector(".menu-nav");
+  const mainNav = document.querySelector(".main-nav");
+  const menuHeader = document.querySelector(".main-header");
+  const navLinks = document.querySelectorAll(".menu-line");
+  menuNav.addEventListener("click", function () {
+    for (let i = 0; i < 3; i++) {
+      navLinks[i].classList.toggle("animateMenu");
+    }
+    mainNav.classList.toggle("nav-height");
+    menuNav.classList.toggle("mhbg");
+    menuHeader.classList.toggle("header-height");
+  });
+};
 
 // Slide in on intersection with Viewport
 function slideInViewportComponent() {
-    const elements = document.querySelectorAll('.fade-in-comp');
+  const elements = document.querySelectorAll(".fade-in-comp");
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('active');
+          entry.target.classList.add("active");
           observer.unobserve(entry.target);
         }
       });
-    }, {
-      threshold: 0.1
-    });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
 
-    elements.forEach(el => {
-      observer.observe(el);
-    });
+  elements.forEach((el) => {
+    observer.observe(el);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    slideInViewportComponent();
-})
+  headerEl.innerHTML = header;
+  asideEl.innerHTML = aside;
+  footerEl.innerHTML = footer;
+  createMenu()
+  slideInViewportComponent();
+});
 
 // Countdown
 const createBlock = (label, value) => {
-	const block = document.createElement("div");
-	block.className = "time-block";
+  const block = document.createElement("div");
+  block.className = "time-block";
 
-	const valueEl = document.createElement("span");
-	valueEl.className = "time-value";
-    
-	valueEl.textContent = value < 10 ? `0${value}` : value;
+  const valueEl = document.createElement("span");
+  valueEl.className = "time-value";
 
-	const labelEl = document.createElement("p");
-	labelEl.className = "time-label";
-	labelEl.textContent = label;
+  valueEl.textContent = value < 10 ? `0${value}` : value;
 
-	if (label === "Seconds") {
-		valueEl.style.animation = "pulse 1s infinite";
-	}
+  const labelEl = document.createElement("p");
+  labelEl.className = "time-label";
+  labelEl.textContent = label;
 
-	block.appendChild(valueEl);
-	block.appendChild(labelEl);
-	return block;
+  if (label === "Seconds") {
+    valueEl.style.animation = "pulse 1s infinite";
+  }
+
+  block.appendChild(valueEl);
+  block.appendChild(labelEl);
+  return block;
 };
 
 const updateCountdown = () => {
-	const now = new Date();
-	const difference = launchDate - now;
+  const launchDate = new Date(2025, 11, 12, 0, 0, 0);
+  const countdownElement = document.querySelector(".countdown-blocks");
+  const now = new Date();
+  const difference = launchDate - now;
 
-	if (difference > 0) {
-		const timeLeft = {
-			Days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-			Hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-			Minutes: Math.floor((difference / 1000 / 60) % 60),
-			Seconds: Math.floor((difference / 1000) % 60)
-		};
+  if (difference > 0) {
+    const timeLeft = {
+      Days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      Hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      Minutes: Math.floor((difference / 1000 / 60) % 60),
+      Seconds: Math.floor((difference / 1000) % 60),
+    };
 
-		countdownElement.innerHTML = "";
-		for (const [label, value] of Object.entries(timeLeft)) {
-			countdownElement.appendChild(createBlock(label, value));
-		}
-	} else {
-		countdownElement.innerHTML = "";
-		document.querySelector(".countdown-title").textContent = "Launch Day!";
-		document.querySelector(".countdown-description").textContent =
-			"The day has arrived!";
+    countdownElement.innerHTML = "";
+    for (const [label, value] of Object.entries(timeLeft)) {
+      countdownElement.appendChild(createBlock(label, value));
+    }
+  } else {
+    countdownElement.innerHTML = "";
+    document.querySelector(".countdown-title").textContent = "Launch Day!";
+    document.querySelector(".countdown-description").textContent =
+      "The day has arrived!";
 
-		const messageBlock = document.createElement("div");
-		messageBlock.className = "time-block expired-message";
-		messageBlock.style.gridColumn = "1 / -1";
-		messageBlock.style.padding = "2rem";
-		messageBlock.textContent = "We're live now!";
+    const messageBlock = document.createElement("div");
+    messageBlock.className = "time-block expired-message";
+    messageBlock.style.gridColumn = "1 / -1";
+    messageBlock.style.padding = "2rem";
+    messageBlock.textContent = "We're live now!";
 
-		countdownElement.appendChild(messageBlock);
-		clearInterval(timer);
-	}
+    countdownElement.appendChild(messageBlock);
+    clearInterval(timer);
+  }
 };
 
 // updateCountdown();
 // const timer = setInterval(updateCountdown, 1000);
 
 // Call for Papers card
-document.querySelectorAll('.view-more-btn').forEach(btn => {
-  btn.addEventListener('click', function () {
+document.querySelectorAll(".view-more-btn").forEach((btn) => {
+  btn.addEventListener("click", function () {
     const list = this.previousElementSibling;
-    list.classList.toggle('expanded');
-    this.innerHTML = list.classList.contains('expanded') ? 'View Less <img src="assets/icons/down.svg" alt="down" style="transform: rotateZ(180deg)">' : 'View More <img src="assets/icons/down.svg" alt="down">';
+    list.classList.toggle("expanded");
+    this.innerHTML = list.classList.contains("expanded")
+      ? 'View Less <img src="assets/icons/down.svg" alt="down" style="transform: rotateZ(180deg)">'
+      : 'View More <img src="assets/icons/down.svg" alt="down">';
   });
-})
-
-// window.addEventListener("scroll", function () {
-//   menuBG();
-// });
+});
