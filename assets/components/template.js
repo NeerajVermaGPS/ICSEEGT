c26f1c2ce9164eea2daa66f435ba708fcbee6edc
nikhil200610 <nikhilchaturvedi7621@gmail.com>
2026-06-23 22:06:54 +0530

    history,fee and commitee

--- assets/components/template.js
+++ assets/components/template.js
@@ -9,13 +9,13 @@ export const header = `
       <div class="header-text text-center">
         <h1 class="tth-1 full-w"><b>Anusandhan National Research Foundation (ANRF)</b>
        <center> Sponsored</center>
-Fifth International Conference on Sustainable Energy, Environment, and Green Technologies (ICSEEGT 2025)</b>
+Sixth International Conference on Sustainable Energy, Environment, and Green Technologies (ICSEEGT 2026)</b>
         </h1>
         <div class="tth-2 full-w column justify-content-center">
           <div class="tth-2">
             Jointly Organized by Department of Mechanical & Civil Engineering<br/>
             <h1 class="tth-1 full-w my-2 fw-bold">Poornima College of Engineering, Jaipur, India</h1>
-            <strong>(Hybrid Mode on December 12-13, 2025)</strong>
+            <strong>(Hybrid Mode on 11–12 December 2026)</strong>
           </div>
         </div>
         <div class="full-w c-row justify-content-center gap-5 mt-4 mt-lg-0">
@@ -109,17 +109,17 @@ export const aside = `
         <h2 class="section-title">Important Dates</h2>
         <div class="column shadow rounded-2 p-4 date-ctnr" id="imp-dates">
           <ul class="list-unstyled dates">
-            <li><span class="date-title">Paper Submission Opens:</span><span class="date-desc">Sunday, August 31, 2025</span></li>
-            <li><span class="date-title">Paper Submission Deadline:</span><span class="date-desc">Sunday, November 30, 2025</span></li>
-            <li><span class="date-title">Acceptance Notification:</span><span class="date-desc">Tuesday, December 2, 2025</span></li>
-            <li><span class="date-title">Registration Process Starts:</span><span class="date-desc">Thursday, October 30, 2025</span></li>
-            <li><span class="date-title">Last Date of Registration:</span><span class="date-desc">Saturday, December 6, 2025</span></li>
-            <li><span class="date-title">Registration with Late Fee:</span><span class="date-desc">Tuesday, December 9, 2025</span></li>
-            <li><span class="date-title">Conference Dates:</span><span class="date-desc">December 12–13, 2025</span></li>
+            <li><span class="date-title">Paper Submission Opens:</span><span class="date-desc">Friday, 15 May 2026</span></li>
+            <li><span class="date-title">Paper Submission Deadline:</span><span class="date-desc">Saturday, 15 August 2026</span></li>
+            <li><span class="date-title">Acceptance Notification:</span><span class="date-desc">Monday, 17 August 2026</span></li>
+            <li><span class="date-title">Registration Process Starts:</span><span class="date-desc">Wednesday, 15 July 2026</span></li>
+            <li><span class="date-title">Last Date of Registration:</span><span class="date-desc">Saturday, 5 December 2026</span></li>
+            <li><span class="date-title">Registration with Late Fee:</span><span class="date-desc">Tuesday, 8 December 2026</span></li>
+            <li><span class="date-title">Conference Dates:</span><span class="date-desc">Friday–Saturday, 11–12 December 2026</span></li>
           </ul>
         </div>
         <h2 class="section-title mt-5">Publishers</h2>
       <div class="full-w mt-5 column shadow rounded-2 p-3 date-ctnr">
          <img src="/assets/images/springer.png" alt="publishers" class="full-w">
-         <p>After peer review by the journal selected papers from ICSEEGT 2024 will be published in a special issue entitled " Recent Environmental and Renewable Energy Innovations for a Sustainable Future'' in Environmental Science and Pollution Research (ESPR) of Springer.</p>
+         <p>After peer review by the journal selected papers from ICSEEGT 2025 will be published in a special issue entitled " Recent Environmental and Renewable Energy Innovations for a Sustainable Future'' in Environmental Science and Pollution Research (ESPR) of Springer.</p>
        </div>`
