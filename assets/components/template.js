0be74ed8cf92ed7d4239cf91bba6c43762f30faf
nikhil200610 <nikhilchaturvedi7621@gmail.com>
2026-06-27 14:26:12 +0530

    ALERT ADEED TO LINKS

--- assets/components/template.js
+++ assets/components/template.js
@@ -7,10 +7,7 @@ export const header = `
         <img src="/assets/images/pce.png" alt="pce">
       </div>
       <div class="header-text text-center">
-        <h1 class="tth-1 full-w"><b>Anusandhan National Research Foundation (ANRF)</b>
-       <center> Sponsored</center>
-Sixth International Conference on Sustainable Energy, Environment, and Green Technologies (ICSEEGT 2026)</b>
-        </h1>
+        <h1 class="tth-1 full-w">Sixth International Conference on Sustainable Energy, Environment, and Green Technologies (ICSEEGT 2026)</h1>
         <div class="tth-2 full-w column justify-content-center">
           <div class="tth-2">
             Jointly Organized by Department of Mechanical & Civil Engineering<br/>
@@ -23,20 +20,20 @@ Sixth International Conference on Sustainable Energy, Environment, and Green Tec
             <img src="/assets/images/pce.png" alt="pce">
           </div>
           <div class="logo-right-s">
-            <img src="/assets/images/icseegt2025.png" alt="ICSEEGT 2025">
+            <img src="/assets/images/ICSEEGT 2026 Logo.png" alt="ICSEEGT 2026">
           </div>
         </div>
       </div>
       <div class="logo-right-l">
-        <img src="/assets/images/icseegt2025.png" alt="ICSEEGT 2025">
+        <img src="/assets/images/ICSEEGT 2026 Logo.png" alt="ICSEEGT 2026">
       </div>
     </div>
     <div class="marquee-container">
       <div class="marquee-content">
-        To download conference brochure <a href="" download target="_blank">click here</a>.
+        To download conference brochure <a href="javascript:void(0)" download onclick="alert('link will be updated soon.')">click here</a>.
         &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-        Paper Submission Link: <a target="_blank" href="">To be updated</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-        Registration Link: <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfkGGUqdccvLL6Q93QRH53_KXNNNErlzxKJFHZicWndigqqnA/viewform">Click here</a>
+        Paper Submission Link: <a target="_blank" href="javascript:void(0)" onclick="alert('link will be updated soon.')">To be updated</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        Registration Link: <a target="_blank" href="javascript:void(0)" onclick="alert('link will be updated soon.')">Click here</a>
       </div>
     </div>
   </header>
@@ -52,7 +49,7 @@ Sixth International Conference on Sustainable Energy, Environment, and Green Tec
     <div class="main-nav c-row">
       <ul class="full-h center c-row">
         <a href="/" class="nrm-btn center">
-          <li>About ICSEEGT 2025</li>
+          <li>About ICSEEGT 2026</li>
         </a>
         <a href="/call-for-papers.html" class="nrm-btn center">
           <li>Call for Papers</li>
@@ -91,7 +88,7 @@ Sixth International Conference on Sustainable Energy, Environment, and Green Tec
 
 export const footer = `
         <div class="container text-center">
-            <p class="mb-0 p-0">&copy; 2025 ICSEEGT. All Rights Reserved.</p>
+            <p class="mb-0 p-0">&copy; 2026 ICSEEGT. All Rights Reserved.</p>
             <p class="mb-0 p-0">Designed and Developed by <a href="https://neerajvermagps.infinityfreeapp.com/">Neeraj
                     Verma</a> (+91 9352298110)</p> 
                     <p class="mb-0 p-0">Managed by <a
@@ -103,7 +100,7 @@ export const footer = `
 
 export const aside = `
         <div class="column shadow rounded-2 p-4 date-ctnr mb-5">
-          <img src="/assets/images/PCE_Logo.png" class="full-w" />
+          <img src="assets/images/PCEAutonomous.jpg" class="full-w" />
         </div>
         <h2 class="section-title">Important Dates</h2>
         <div class="column shadow rounded-2 p-4 date-ctnr" id="imp-dates">
@@ -117,8 +114,8 @@ export const aside = `
             <li><span class="date-title">Conference Dates:</span><span class="date-desc">Friday–Saturday, 11–12 December 2026</span></li>
           </ul>
         </div>
-        <h2 class="section-title mt-5">Publishers</h2>
+       <!-- <h2 class="section-title mt-5">Publishers</h2>
       <div class="full-w mt-5 column shadow rounded-2 p-3 date-ctnr">
-         <img src="/assets/images/springer.png" alt="publishers" class="full-w">
-         <p>After peer review by the journal selected papers from ICSEEGT 2025 will be published in a special issue entitled " Recent Environmental and Renewable Energy Innovations for a Sustainable Future'' in Environmental Science and Pollution Research (ESPR) of Springer.</p>
+         <img src="void" alt="publishers" class="full-w"> 
+         <p>TO BE UPDATED</p>-->
        </div>`
